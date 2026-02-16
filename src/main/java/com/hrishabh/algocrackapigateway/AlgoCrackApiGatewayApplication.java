21402545f23af8b5ea18e81aac048e42fa8a8465
Hrishabh Joshi <hrishabhjoshi123@gmail.com>
2026-02-16 14:00:45 +0530

    feat: Initialize Spring Boot API Gateway project with Gradle build, Spring Cloud Gateway, and basic application structure.

---
+++ src/main/java/com/hrishabh/algocrackapigateway/AlgoCrackApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocrackapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoCrackApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoCrackApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrishabh/algocrackapigateway/AlgoCrackApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrishabh.algocrackapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoCrackApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
